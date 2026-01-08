c058b8144fcd7b661c88ee8dc27ca669fee6842a
Ixlosbek <bekerkinov2004@gmail.com>
2026-01-08 18:55:40 +0500

    update

--- src/main/database/entity/Task.ts
+++ src/main/database/entity/Task.ts
@@ -1,11 +1,7 @@
-/**
- * TaskEntity - Bu bazadagi 'tasks' jadvalining nusxasi.
- * Har bir qator mana shu strukturada saqlanadi.
- */
 export interface TaskEntity {
-    id?: number;          // Avtomatik oshib boruvchi ID (Optional, chunki yaratishda hali bo'lmaydi)
-    title: string;        // Vazifa sarlavhasi
-    description: string;  // Vazifa haqida batafsil
-    is_completed: number; // SQLite-da boolean yo'q, shuning uchun 0 yoki 1 ishlatamiz
-    created_at: string;   // Yaratilgan sana (ISO string)
+    id?: number;          
+    title: string;       
+    description: string;  
+    is_completed: number; 
+    created_at: string;   
 }
