cefeb49632b6a61983333df8e9bec7b7611dda64
Ixlosbek <bekerkinov2004@gmail.com>
2026-01-08 16:03:07 +0500

    database object created

--- src/main/database/database.ts
+++ src/main/database/database.ts
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import path from "path";
+import path, { resolve } from "path";
 import { app } from "electron";
 
 const dbPath = path.join(app.getPath("userData"), "pro_tasks.db");
@@ -10,3 +10,28 @@ const db = new sqlite3.Database(dbPath, (err) => {
     console.log("SQLite bazasiga ulanish muvofaqiyatli boldi");
   }
 });
+
+export function initDatabase(): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const sql = `
+        CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT,
+        is_completed INTEGER DEFAULT 0,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP       
+        )
+        `;
+
+    db.run(sql, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(`Task tekshirildi/yaratildi`);
+        resolve();
+      }
+    });
+  });
+}
+
+export default db;
